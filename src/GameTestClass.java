5052625374f933f19579ff86387cc211cd6f63a1
mhopps108 <MHopps@Matts-Retina-MBP.local>
2015-03-04 21:39:49 -0800

    test1

---
+++ src/GameTestClass.java
@@ -0,0 +1,17 @@
+
+
+// comments go here
+// test comment
+// my name is matt
+
+public class GameTestClass {
+
+	public GameTestClass() {
+		System.out.println("test game");
+	}
+
+	public static void main(String[] args) {
+		GameTestClass gtc = new GameTestClass();
+	}
+
+}
