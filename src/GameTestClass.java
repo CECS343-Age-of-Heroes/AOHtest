8133de9286bc62ecdf4f14fecdc3cc961e164b0e
mhopps108 <MHopps@dhcp-39-89-193.dhcpint.csulb.edu>
2015-03-09 21:02:09 -0700

    ass

--- src/GameTestClass.java
+++ src/GameTestClass.java
@@ -1,4 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 // comments go here
 // test comment
@@ -6,16 +12,32 @@
 // another commit
 // I hope this works
 
-public class GameTestClass {
+public class GameTestClass extends JFrame {
 
+	private JPanel p = new JPanel();
+	
 	public GameTestClass() {
-		System.out.println("test game");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //mediaFrame.setBackground(Color.GREEN.darker().darker());
+		setResizable(false);
+		setLocationRelativeTo(null);
+		JPanel p = new JPanel();
+		p.setPreferredSize(new Dimension(1200, 800));
+		p.add(new JPanel(new Dimension(12)))
+		
+		//mediaFrame.getContentPane().add(new MLPanel());
+		getContentPane().add(p);
+		pack();
+		setVisible(true);
 	}
 
+
 	public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+		//System.out.println("Hello World!");
 		new GameTestClass();
-		System.out.println(Poop(3, 2));
+		//System.out.println(Poop(3, 2));
+		
 	}
 
 	public static int Poop(int x, int y) {
